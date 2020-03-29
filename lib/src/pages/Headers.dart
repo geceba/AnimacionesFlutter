1301c229c9d8fad547a472cae39fdedd35927d5e
Quetzalcoalt17 <gerardocetzal@grupogit.com>
2020-03-28 22:20:20 -0600

    Bordes redondeados

--- lib/src/pages/Headers.dart
+++ lib/src/pages/Headers.dart
@@ -2,12 +2,11 @@ import 'package:animation/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
 
 class HeadersPage extends StatelessWidget {
-  const HeadersPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HeaderCuadrado()
+      body: HeaderBordesRedondeados()
     );
   }
 }
