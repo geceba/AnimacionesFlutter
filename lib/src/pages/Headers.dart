8753538cd0ac9f6ed6dd20af87cfc2332eb9d34f
Quetzalcoalt17 <gerardocetzal@grupogit.com>
2020-03-30 00:09:57 -0600

    🎸 more custom headers

--- lib/src/pages/Headers.dart
+++ lib/src/pages/Headers.dart
@@ -6,7 +6,7 @@ class HeadersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HeaderTriangular()
+      body: HeaderWave()
     );
   }
 }
--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -31,13 +31,10 @@ class HeaderDiagonal extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       //color: Color(0xff615AAB),
-      child: CustomPaint(
-        painter: _HeaderDiagonalPainter()
-      ),
+      child: CustomPaint(painter: _HeaderDiagonalPainter()),
     );
   }
 }
-
 
 class _HeaderDiagonalPainter extends CustomPainter {
   @override
@@ -62,8 +59,8 @@ class _HeaderDiagonalPainter extends CustomPainter {
     // TODO: implement shouldRepaint
     return true;
   }
-
 }
+
 class HeaderTriangular extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -71,13 +68,10 @@ class HeaderTriangular extends StatelessWidget {
       height: double.infinity,
       width: double.infinity,
       //color: Color(0xff615AAB),
-      child: CustomPaint(
-        painter: _HeaderTriangularPainter()
-      ),
+      child: CustomPaint(painter: _HeaderTriangularPainter()),
     );
   }
 }
-
 
 class _HeaderTriangularPainter extends CustomPainter {
   @override
@@ -92,6 +86,119 @@ class _HeaderTriangularPainter extends CustomPainter {
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
 
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class HeaderPico extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(painter: _HeaderPicoPainter()),
+    );
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    final paint = new Paint();
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;
+
+    final path = new Path();
+    path.lineTo(0, size.height * 0.25);
+    path.lineTo(size.width * 0.50, size.height * 0.30);
+    path.lineTo(size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class HeaderCurvo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(painter: _HeaderCurvoPainter()),
+    );
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    final paint = new Paint();
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;
+
+    final path = new Path();
+    path.lineTo(0, size.height * 0.25);
+    //path.lineTo(size.width, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.5, size.height * 0.4, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
+
+class HeaderWave extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      //color: Color(0xff615AAB),
+      child: CustomPaint(painter: _HeaderWavePainter()),
+    );
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    final paint = new Paint();
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;
+
+    final path = new Path();
+    path.lineTo(0, size.height * 0.25);
+    //path.lineTo(size.width, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.25, size.height * 0.30, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(
+        size.width * 0.75, size.height * 0.20, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
     canvas.drawPath(path, paint);
   }
 
